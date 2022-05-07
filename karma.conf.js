d285b9616364cf566495654a282afc7508065181
burndaflame <6634023+burndaflame@users.noreply.github.com>
2022-05-07 23:22:14 +0200

    renamed output

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sw5e-sot-g'),
+      dir: require('path').join(__dirname, './coverage/sw5e-sotg'),
       subdir: '.',
       reporters: [
         { type: 'html' },
